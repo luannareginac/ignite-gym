9416127d3acfdc6a760739a42d4791ff0a9dab6c
Luanna Regina Costa <luannareginac@gmail.com>
2025-01-03 11:30:56 -0300

    home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,22 @@
 import { ExerciseCard } from "@components/ExerciseCard";
 import Group from "@components/Group";
+import { useNavigation } from '@react-navigation/native'
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
 import { HomeHeader } from "@components/HomeHeader";
 import {  Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
 import { useState } from "react";
 import { FlatList } from 'react-native'
 
 export function Home(){
+  
    const[exercises, setExercises] =useState(['Puxada frontal','Remada curvada','Remanda unilateral','Levantamento terra'])
    const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
    const [groupSelected, setGroupSelected] = useState("Costas")
+   const navigation = useNavigation<AppNavigatorRoutesProps>()
+   function handleOpenExerciseDetails() {
+     navigation.navigate('exercise')
+   }
+ 
    return(
       <VStack flex={1}>
         <HomeHeader/>
@@ -36,7 +44,9 @@ export function Home(){
         <FlatList 
         data={exercises}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <ExerciseCard />}
+        renderItem={() => (
+          <ExerciseCard onPress={handleOpenExerciseDetails} />
+        )}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 20 }}
         
